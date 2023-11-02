1143b98c33e7f9856c075cacb2ab039c49b3c2e7
kevinf_cakerycupcollective <tech@cravethecup.com>
2023-11-02 10:52:51 -0500

    Fix form in converter.js

--- converter.js
+++ converter.js
@@ -2,7 +2,7 @@ const inputField = document.getElementById('input-temp');
 const fromUnitField = document.getElementById('input-unit');
 const toUnitField = document.getElementById('output-unit');
 const outputField = document.getElementById('output-temp');
-const form = document.getElementById('converter');
+const frm = document.getElementById('converter');
 
 function convertTemp(value, fromUnit, toUnit) {
   if (fromUnit === 'c') {
@@ -32,7 +32,7 @@ function convertTemp(value, fromUnit, toUnit) {
   throw new Error('Invalid unit');
 }
 
-form.addEventListener('input', () => {
+frm.addEventListener('input', () => {
   const inputTemp = parseFloat(inputField.value);
   const fromUnit = fromUnitField.value;
   const toUnit = toUnitField.value;
